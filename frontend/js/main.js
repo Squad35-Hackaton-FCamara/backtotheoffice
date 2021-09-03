454cd1690bcebe0f1a53d762497d0f27be038f0b
Nathalia Luiza Schedler Calza <nlscalza@gmail.com>
2021-09-03 16:22:02 -0300

    inicia desenvolveimento tela cadastro web

---
+++ frontend/js/main.js
@@ -0,0 +1,8 @@
+/* const cardIcone = document.querySelectorAll(".input-group-text");
+const iconeSenha = cardIcone.firstElementChild;
+
+addEventListener.iconeSenha("click", () => {
+    if (iconeSenha.classList == ".fa .fa-eye-slash") {
+        backdrop.classList.add("visible")
+    }
+}) */
