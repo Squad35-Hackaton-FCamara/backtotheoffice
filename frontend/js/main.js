bb5c720f7593459afa92b747811679844c9a7041
Nathalia Luiza Schedler Calza <nlscalza@gmail.com>
2021-09-05 20:01:21 -0300

    implementa funcionalidade ao ícone exibir/ocultar senha tela de cadastro

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,8 +1,33 @@
-/* const cardIcone = document.querySelectorAll(".input-group-text");
-const iconeSenha = cardIcone.firstElementChild;
+const cardIconeSenha = document.getElementById("iconeSenha");
+const iconeSenha = cardIconeSenha.firstElementChild;
+const cardIconeRepetirSenha = document.getElementById("iconeRepetirSenha");
+const iconeRepetirSenha = cardIconeRepetirSenha.firstElementChild;
+const inputSenha = document.getElementById("senha");
+const inputRepetirSenha = document.getElementById("repetirSenha")
 
-addEventListener.iconeSenha("click", () => {
-    if (iconeSenha.classList == ".fa .fa-eye-slash") {
-        backdrop.classList.add("visible")
-    }
-}) */
+const passwordIconClickHandler = () => {
+    if (inputSenha.type == "password") {
+        inputSenha.type = "text";
+        iconeSenha.classList.remove( "fa-eye-slash" );
+        iconeSenha.classList.add( "fa-eye" );
+      } else {
+        inputSenha.type = "password";
+        iconeSenha.classList.add( "fa-eye-slash" );
+        iconeSenha.classList.remove( "fa-eye" );
+      }
+}
+
+const repeatPasswordIconClickHandler = () => {
+    if (inputRepetirSenha.type == "password") {
+        inputRepetirSenha.type = "text";
+        iconeRepetirSenha.classList.remove("fa-eye-slash");
+        iconeRepetirSenha.classList.add("fa-eye");
+      } else {
+        inputRepetirSenha.type = "password";
+        iconeRepetirSenha.classList.add("fa-eye-slash");
+        iconeRepetirSenha.classList.remove("fa-eye");
+      }
+}
+
+iconeSenha.addEventListener("click", passwordIconClickHandler);
+iconeRepetirSenha.addEventListener("click", repeatPasswordIconClickHandler);
