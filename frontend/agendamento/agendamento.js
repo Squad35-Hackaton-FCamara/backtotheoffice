03cf4dd9608d7deb19cfde60e138cac2f88f2d7c
Nathalia Luiza Schedler Calza <nlscalza@gmail.com>
2021-09-08 12:06:49 -0300

    finaliza css tela de agendamento

--- frontend/agendamento/agendamento.js
+++
@@ -1,6 +0,0 @@
-var datepickers = [].slice.call(document.querySelectorAll('[data-datepicker]'))
-var datepickersList = datepickers.map(function (el) {
-    return new Datepicker(el, {
-        buttonClass: 'btn'
-    });
-});
